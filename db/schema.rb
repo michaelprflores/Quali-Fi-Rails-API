ed09010b9dbcb25d501f9e318c1c4e2d46147a87
Mike Flores <michaelprflores@gmail.com>
2016-10-31 13:24:39 -0400

    Opening and Applicant data are seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031154150) do
+ActiveRecord::Schema.define(version: 20161031171527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applicants", force: :cascade do |t|
+    t.string  "name"
+    t.string  "skills"
+    t.string  "education"
+    t.integer "years_exp"
+    t.boolean "local"
+  end
 
   create_table "openings", force: :cascade do |t|
     t.string  "opening_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,15 @@ Applicant.delete_all
 full_stack_developer = Opening.create({
   opening_name: "Full Stack Developer",
   skills: ["JavaScript", "Rails"],
-  education: "Bachelor's Degree",
+  education: "Bachelors Degree",
   years_exp: 5,
   local: false
 })
 
 first_applicant = Applicant.create({
   name: "Bruce Wayne",
-  skills: ["JavaScript", "PHP"]
-  education: "Bachelor's Degree",
+  skills: ["JavaScript", "PHP"],
+  education: "Bachelors Degree",
   years_exp: 12,
   local: true
 })
