54683e02675193b456e9b52d0963e1fb81cc5f3e
Mike Flores <michaelprflores@gmail.com>
2016-10-31 14:30:43 -0400

    made edits to file before running db:migrate

--- db/migrate/20161031182505_create_openings_applicants_join_table.rb
+++ db/migrate/20161031182505_create_openings_applicants_join_table.rb
@@ -1,12 +1,11 @@
 class CreateOpeningsApplicantsJoinTable < ActiveRecord::Migration[5.0]
   def change
-      create_table :openings_applicants, id: false do |t|
-        t.integer :opening_id
-        t.integer :applicant_id
-      end
-  
-      add_index :openings_applicants, :opening_id
-      add_index :openings_applicants, :applicant_id
+    create_table :openings_applicants, id: false do |t|
+      t.integer :opening_id
+      t.integer :applicant_id
     end
+    
+    add_index :openings_applicants, :opening_id
+    add_index :openings_applicants, :applicant_id
   end
 end
