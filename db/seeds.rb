0d32180639c1334ca6eaec1c1f908c2681fdae5b
Mike Flores <michaelprflores@gmail.com>
2016-10-31 13:13:26 -0400

    created the Applicant model and some seed data for it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 Opening.delete_all
+Applicant.delete_all
 
 full_stack_developer = Opening.create({
-  opening_name: "Hall & Oates",
+  opening_name: "Full Stack Developer",
   skills: ["JavaScript", "Rails"],
   education: "Bachelor's Degree",
   years_exp: 5,
   local: false
+})
+
+first_applicant = Applicant.create({
+  name: "Bruce Wayne",
+  skills: ["JavaScript", "PHP"]
+  education: "Bachelor's Degree",
+  years_exp: 12,
+  local: true
 })
